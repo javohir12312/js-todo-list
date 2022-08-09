08e079d92f71e8c8594b687c631a34a75e0bc99f
javohir12312 <abduxalilovjavohir393@gmail.com>
2022-08-10 00:01:54 +0300

    full

---
+++ js/main.js
@@ -0,0 +1,34 @@
+var list = document.querySelector('ul');
+
+list.addEventListener('click', function (ev) {
+    if(ev.target.tagName === "LI") {
+       ev.target.classList.toggle('checked');
+    } else if(ev.target.tagName === "SPAN") {
+       var div = ev.target.parentNode;
+       div.remove();
+    }
+});
+
+function newElement() {
+
+    var li = document.createElement('li');
+    var inputValue = document.getElementById('toDoEl').value;
+    var t = document.createTextNode(inputValue);
+
+    li.appendChild(t);
+
+    if(inputValue == "") {
+       list.textContent = "Nima kerak ekanligini kiriting";
+    } 
+    else {
+       document.getElementById('list').appendChild(li);
+    }
+
+
+    document.getElementById('toDoEl').value = "";
+    var span = document.createElement('SPAN');
+    var txt = document.createTextNode("X");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+}
